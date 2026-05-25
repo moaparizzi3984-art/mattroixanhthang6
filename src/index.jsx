ef5cacbee9fcdd806f151590c27c045782e80184
Nguyễn Nguyên Giáp <103838138+megait004@users.noreply.github.com>
2026-05-26 05:11:56 +0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import '@/assets/css/bootstrap.min.css';
 import '@/assets/css/style.css';
 import { RouterProvider } from 'react-router';
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <RouterProvider router={router} />
+        <Analytics />
     </React.StrictMode>
 );
 
